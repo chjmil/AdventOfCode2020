fc65d790bafd6de4423263c0f95ec5c63efbdf86
chjmil <christopher.miller0301@gmail.com>
2021-05-28 16:34:21 -0400

    Adding Day2 part 2 code
    I added the second part of Day 2's exercise, which was surprisingly straightforward. I was expecting it to be a little bit more tricky, but all I needed to do was search through the password to find the characters at the specified position.
    
    I've included the output into the Day2 directory.

--- AdventOfCode2020/Day2/password_validator.py
+++ AdventOfCode2020/Day2/password_validator.py
@@ -9,12 +9,30 @@ def password_validator_part1(rule, value):
     :param value (str): The password in question
     :return (rule, value) if the password is valid, else None
     """
-    # break up the rule into it's parts
+    # break up the rule into its components
     number_range, c = rule.split(' ')
     number_range = number_range.replace('-', ',')
     validation_regex = re.compile(f"^(?:[^{c}]*{c}){{{number_range}}}[^{c}]*$")
 
-    return validation_regex.match(value)
+    return validation_regex.match(value) is not None
+
+def password_validator_part2(rule, value):
+    """
+    Check to see if the value follows the rule. If so, return them.
+    :param rule (str): The rule that the value must follow
+    :param value (str): The password in question
+    :return (rule, value) if the password is valid, else None
+
+    """
+    # break up the rule into its components
+    number_range, c = rule.split(' ')
+    small, big = number_range.split('-')
+
+    # check to see if the expected character is at the specified location(s)
+    small_check = c == value[int(small)-1]
+    big_check = c == value[int(big)-1]
+
+    return True and (small_check ^ big_check)
 
 
 def main(input_list_path: str):
@@ -31,7 +49,7 @@ def main(input_list_path: str):
     with open(input_list_path, 'r') as f:
         # List comprehension to break lines into lists containing the rule and the value. 
         # also remove the \n
-        input = [x.replace('\n', '').split(':') for x in f.readlines()]
+        input = [x.replace('\n', '').split(': ') for x in f.readlines()]
     
     # List comprehension to store all valid passwords
     valid_passwords = []
@@ -43,6 +61,9 @@ def main(input_list_path: str):
     print(f"Valid passwords: {valid_passwords}")
     print(f"Number of Valid Passwords: {len(valid_passwords)}")
 
+    valid_passwords = []
+    for combo in input:
+        valid_passwords.append(combo) if password_validator_part2(*combo) else None
 
     # Print the valid passwords and corresponding rules
     print("--------------------PART 2-----------------------")
