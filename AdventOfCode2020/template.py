0770c759ec5431999ce6b9195cf50330efc568ae
chjmil <christopher.miller0301@gmail.com>
2021-06-14 14:44:23 -0400

    Updating template file format
    I'm updating the template python file so that
    it uses a class-based structure. I feel like this
    style will be better since I can store the input
    in a class variable instead of having to pass around
    the input list to every function. Hopefully this is
    a useful change and makes it easier in the future.
    
    Also removing some leftover comments from previous python
    files.

--- AdventOfCode2020/template.py
+++ AdventOfCode2020/template.py
@@ -4,30 +4,62 @@ Template file to make it easier to copy the basic structure.
 import argparse
 import os
 
-def main(input_list_path: str):
-    """
-    Parse through the provided file for ...
-    :param input_list_path (str): Path to the input file
-    """
-    # Verify the file exists
-    if not os.path.exists(input_list_path):
-        print(f"Input file does not exist: {input_list_path}")
-        exit(1)
+class adapter_array():
 
-    # Parse the file and store it in a list
-    with open(input_list_path, 'r') as f:
-        # List comprehension to ...
-        input = [x for x in f.readlines()]
+    def __init__(self, input_list_path: str):
+        """
+        Handle the input file and store the contents in a class variable
+        :param input_list_path (str): Path to the input file
+        """
+        # Verify the file exists
+        if not os.path.exists(input_list_path):
+            print(f"Input file does not exist: {input_list_path}")
+            exit(1)
 
-    # Print the solution
-    print("--------------------PART 1-----------------------")
-    print(f"Answer: {1}")
+        # Parse the file and store it in a list
+        with open(input_list_path, 'r') as f:
+            # Store the lines of input into the self.input var
+            self.input = f.readlines()
 
+    def part_one_function(self):
+        """
+        Figure out ...
+        """
+        # Print the solution
+        print("--------------------PART 1-----------------------")
+        print(f"Answer: {1}")
+
+
+    def part_two_function(self):
+        """
+        Figure out ...
+        """
+        
+        # Print the solution
+        print("--------------------PART 2-----------------------")
+        print(f"Answer: {1}")
+
+
+    def main(self, part_one: bool = True, part_two: bool = True):
+        """
+        Run part one, part two, or both and output the answers.
+        """
+        if part_one:
+            self.part_one_function()
+        
+        if part_two:
+            self.part_two_function()
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the program.")
     parser.add_argument("--input-path", help="Path to the input file", 
                         type=str, required=True)
+    parser.add_argument("--skip-one", help="Skip part 1", 
+                        action='store_true', required=False, default=False)
+    parser.add_argument("--skip-two", help="Skip part 2", 
+                        action='store_true', required=False, default=False)
 
     args = parser.parse_args()
-    main(input_list_path=args.input_path)
+    adapter = adapter_array(input_list_path=args.input_path)
+    adapter.main(part_one=not args.skip_one, part_two=not args.skip_two)
