2238152a76c552995f1756f545246abcbfaf3660
chjmil <christopher.miller0301@gmail.com>
2021-06-18 00:15:56 -0400

    Partial work on Day 11 part 1
    I've completed most of Day 11's part 1. I am getting
    the wrong answer, but it looks like it's just an issue
    with some indices. I need to take another look after a good
    night's sleep to see where I messed things up.

--- Day11/seating_system.py
+++ Day11/seating_system.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-class template():
+class seating_system():
 
     def __init__(self, input_list_path: str):
         """
@@ -15,16 +15,74 @@ class template():
 
         # Parse the file and store it in a list
         with open(input_list_path, 'r') as f:
-            # Store the lines of input into the self.input var
-            self.input = f.readlines()
+            # Store the lines of input into the self.grid var
+            self.grid = [x.strip() for x in f.readlines()]
+        # Number of rows
+        self.row_len = len(self.grid)
+        # Number of seats (columns)
+        self.col_len = len(self.grid[0])
+    
+    def step(self, id: tuple):
+        """
+        Simulate the next round to see if the seat
+        changes or stays the same.
+        :param id (tuple): (x, y) coordinate
+        """
+        x_range = [max(id[0]-1, 0), min(id[0]+1, self.col_len-2)]
+        y_range = [max(id[1]-1, 0), min(id[1]+1, self.row_len-2)+1]
+        # print(f"x{x_range} and y{y_range}: {self.grid[id[1]][id[0]]}")
+        is_occupied = self.grid[id[1]][id[0]] == '#'
+
+        # Wacky way to count the number of neighbors
+        neighbors = ""
+        for row in range(*y_range):
+            neighbors += self.grid[row][x_range[0]:x_range[1]+1]
+        num_neighbors = neighbors.count('#')
+        if is_occupied:
+            # if neighbors > 3, return L. else return #
+            # subtract 1 since the seat in question is occupied
+            if num_neighbors -1 > 3:
+                return 'L'
+            return '#'
+        else:
+            # if neighbors > 1, return L. else return #
+            if num_neighbors == 0:
+                return '#'
+            return 'L'
+        
 
     def part_one_function(self):
         """
-        Figure out ...
+        Figure out the number of occupied seats after the simulation
+        has settled.
         """
-        # Print the solution
+        # step through until the previous grid is identical to the new one
+        new_grid = []
+        while True:
+            # For each row
+            for row in range(self.row_len):
+                updated_row = ""
+                # for each seat in each row
+                for seat in range(self.col_len):
+                    if self.grid[row][seat] == '.':
+                        updated_row += '.'
+                    else:
+                        updated_row += self.step((seat, row))
+                new_grid.append(updated_row)
+                print(updated_row)
+            
+            # check to see if the grids are equal
+            if self.grid == new_grid:
+                break
+            self.grid = new_grid
+            new_grid = []
+            print("---------------------")
+            
+
+        # Combine all of the rows into a single string and count
+        # the occupied seats
         print("--------------------PART 1-----------------------")
-        print(f"Answer: {1}")
+        print(f"Answer: {''.join(self.grid).count('#')}")
 
 
     def part_two_function(self):
@@ -58,5 +116,5 @@ if __name__ == "__main__":
                         action='store_true', required=False, default=False)
 
     args = parser.parse_args()
-    adapter = template(input_list_path=args.input_path)
-    template.main(part_one=not args.skip_one, part_two=not args.skip_two)
+    seats = seating_system(input_list_path=args.input_path)
+    seats.main(part_one=not args.skip_one, part_two=not args.skip_two)
